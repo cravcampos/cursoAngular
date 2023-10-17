25a08c76b48d832462f16d85712c09b65189b999
Victor Campos <cravcampos@gmail.com>
2023-10-17 00:39:05 -0500

    Creacion de proyecto vite para introduccion a TypeScript

---
+++ 01-TypeScript/01-typescript-intro/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  Hola mundo
+`
+
+console.log("Victor Campos")
+
+
