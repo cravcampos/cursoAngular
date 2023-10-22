c9b2642274c6ffb019d94cd9af081e870d4acf89
Victor Campos <cravcampos@gmail.com>
2023-10-22 01:42:57 -0500

    priorizar la composicion sobre la herencia

--- 01-TypeScript/01-typescript-intro/src/topics/09-classes.ts
+++ 01-TypeScript/01-typescript-intro/src/topics/09-classes.ts
@@ -1,21 +1,24 @@
 export class Person{
 
     constructor(
-        public name: string,
+        public firstName: string,
+        public lastName: string,
         private address: string = 'No address'
 
     ){}
 }
 
-export class Hero extends Person{
+export class Hero {
     constructor(
         public alterEgo: string,
         public age: number,
         public realName: string,
+        public person: Person,
     ){
-        super(realName,'Colombia');
+        
     }
 }
 
-const ironman = new Hero('Ironman',45,'Tony');
-console.log(ironman);
+const tony = new Person('Tony', 'Stark', 'New York');
+const ironman = new Hero('Ironman',45,'Tony', tony);
+console.log({ironman});
