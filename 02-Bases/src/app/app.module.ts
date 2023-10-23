6adb2e09b6587ac252205eabdea29ba05c3b3864
Victor Campos <cravcampos@gmail.com>
2023-10-23 16:41:57 -0500

    creando componente counter

--- 02-Bases/src/app/app.module.ts
+++ 02-Bases/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
