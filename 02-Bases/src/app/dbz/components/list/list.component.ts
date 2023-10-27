7b12619bfd2443459e1467544351c0bb7daaa48d
Victor Campos <cravcampos@gmail.com>
2023-10-26 22:56:50 -0500

    Agregando uuid a cada uno de nuestros personajes

--- 02-Bases/src/app/dbz/components/list/list.component.ts
+++ 02-Bases/src/app/dbz/components/list/list.component.ts
@@ -15,8 +15,11 @@ export class ListComponent {
   }];
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
-  onDeleteCharacter(index: number): void {
-    this.onDelete.emit(index);
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    console.log(id);
+    this.onDelete.emit(id);
   }
 }
