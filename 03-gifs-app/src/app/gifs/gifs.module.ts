5701b1092ac7eed012c3fbe783a1ee4401969e7b
Victor Campos <cravcampos@gmail.com>
2023-10-27 23:53:35 -0500

    diseño del buscador y del listado

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import {SearchBoxComponent} from "./components/search-box/search-box.component";
+import { CardListComponent } from './components/card-list/card-list.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
+  exports: [
+    HomePageComponent
+  ],
   imports: [
     CommonModule
   ]
