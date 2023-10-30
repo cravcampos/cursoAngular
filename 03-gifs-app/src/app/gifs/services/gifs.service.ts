c56524c8946dab5ff3446fbb961717911ab36d2b
Victor Campos <cravcampos@gmail.com>
2023-10-29 23:01:33 -0500

    Usando fetch para traer la información de la api

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -27,12 +27,17 @@ export class GifsService {
     this._tagsHistory = this.tagsHistory.splice(0,10);
   }
 
-  searchTag(tag: string): void {
+  async searchTag(tag: string):Promise<void> {
 
     // validando que la búsqueda no este vacía
     if (tag.length === 0) return;
 
     // Usando método para organizar los tag y limitarlos a 10
     this.organizeHistory(tag);
+
+    //Usando fetch para realizar la petición al api
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=lSekN7UXwIHfk3Ps1NoXVvaqoDnMIeOr&q=perros&limit=10')
+      .then(resp=> resp.json())
+      .then(data => console.log(data));
   }
 }
